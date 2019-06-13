d18f8a2e0518e82e326217b727674575eb6efc58
Max Rybalchenko <maksimry@rabota.ua>
2019-06-13 19:38:37 +0300

    Info-clock added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,15 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 
 import { AppComponent } from './app.component'
-import { PersonalDataComponent } from './personal-data/personal-data.component'
+import { PersonalDataComponent } from './components/personal-data/personal-data.component'
 import { HttpClientModule } from '@angular/common/http'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module'
-import { HomePageComponent } from './home-page/home-page.component'
+import { HomePageComponent } from './home-page/home-page.component';
+import { InfoBlockComponent } from './components/info-block/info-block.component'
 
 @NgModule({
-  declarations: [AppComponent, PersonalDataComponent, HomePageComponent],
+  declarations: [AppComponent, PersonalDataComponent, HomePageComponent, InfoBlockComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
