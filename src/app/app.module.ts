886e6abf1c5b1c7196093bc9eac15829319d9b10
Max Rybalchenko <maksimry@rabota.ua>
2019-06-13 19:48:04 +0300

    style + basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { HttpClientModule } from '@angular/common/http'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module'
 import { HomePageComponent } from './home-page/home-page.component';
-import { InfoBlockComponent } from './components/info-block/info-block.component'
+import { InfoBlockComponent } from './components/info-block/info-block.component';
+import { NavigationMenuComponent } from './components/navigation-menu/navigation-menu.component';
+import { HeaderComponent } from './shared/header/header.component'
 
 @NgModule({
-  declarations: [AppComponent, PersonalDataComponent, HomePageComponent, InfoBlockComponent],
+  declarations: [AppComponent, PersonalDataComponent, HomePageComponent, InfoBlockComponent, NavigationMenuComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
