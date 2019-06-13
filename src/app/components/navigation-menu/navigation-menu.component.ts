d0d43e4b21631efd7a8ed0e8976b77dae90d73ae
Max Rybalchenko <maksimry@rabota.ua>
2019-06-13 20:12:45 +0300

    header & navigation added

--- src/app/components/navigation-menu/navigation-menu.component.ts
+++ src/app/components/navigation-menu/navigation-menu.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation-menu.component.css']
 })
 export class NavigationMenuComponent implements OnInit {
+  public readonly menuList = [
+    { id: 'red', name: 'red' },
+    { id: 'green', name: 'green' },
+    { id: 'blue', name: 'blue' },
+    { id: 'yellow', name: 'yellow' },
+    { id: 'grey', name: 'grey' }
+  ]
 
   constructor() { }
 
