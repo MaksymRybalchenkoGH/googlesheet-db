178a62cc4b34619748b8e79b350f7b6759a0b04f
Max Rybalchenko <maksimry@rabota.ua>
2019-06-13 19:25:43 +0300

    prettier fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-import { AppComponent } from './app.component';
-import { PersonalDataComponent } from './personal-data/personal-data.component';
-import {HttpClientModule} from '@angular/common/http';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms'
-import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component'
+import { PersonalDataComponent } from './personal-data/personal-data.component'
+import { HttpClientModule } from '@angular/common/http'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { AppRoutingModule } from './app-routing.module'
 import { HomePageComponent } from './home-page/home-page.component'
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PersonalDataComponent,
-    HomePageComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule,
-  ],
+  declarations: [AppComponent, PersonalDataComponent, HomePageComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/personal-data/personal-data.component.ts
+++ src/app/personal-data/personal-data.component.ts
@@ -7,28 +7,21 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
   styleUrls: ['./personal-data.component.css']
 })
 export class PersonalDataComponent implements OnInit {
-
   public form: FormGroup
 
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
       name: new FormControl('', Validators.required),
-      email: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required)
     })
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   public onSubmit(formData): void {
     const scriptUrl = 'https://script.google.com/macros/s/AKfycbza5ccExwFnhpmI2to2-5JJxUMhtKmI6QDf3MK3Y7S7HSAzD_ej/exec'
     const url = `${scriptUrl}?callback=ctrlq&name=${this.formName}&email=${this.formEmail}`
-    fetch(url, {mode: 'no-cors'})
-      .then(
-      data => console.log('success', data) ,
-      error => console.log('error', error)
-      )
-
+    fetch(url, { mode: 'no-cors' }).then(data => console.log('success', data), error => console.log('error', error))
   }
   private get formName(): string {
     return this.form.controls.name.value
@@ -36,5 +29,4 @@ export class PersonalDataComponent implements OnInit {
   private get formEmail(): string {
     return this.form.controls.email.value
   }
-
 }
